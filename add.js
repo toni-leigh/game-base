73d94256041b3821a4c695587a774543ffa33eb2
Toni Leigh Sharpe <tonileigh303@gmail.com>
2018-02-19 19:55:30 +0000

    Initialise basic JS
    * Git ignore node_modules
    * Basic test with Jest
    * Check lodash works

---
+++ add.js
@@ -0,0 +1,7 @@
+var _ = require('lodash');
+
+function add(a, b) {
+	return _.add(a, b);
+}
+
+module.exports = add;
---
+++ test.test.js
@@ -0,0 +1,3 @@
+const add = require('./add');
+
+test('add', function() { expect(add(1, 2)).toEqual(3); });
